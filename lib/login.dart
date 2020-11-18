d63afbaa5d3df6d34c8efe746760eeb892462a51
akhilpasumarthi <pasumarthiakhil66@gmail.com>
2020-11-19 00:39:50 +0530

    added authentication

--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:streecare/EnterMobile.dart';
+import 'package:streecare/LoginScreen.dart';
 import 'login.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      home: LoginScreen(),
     );
   }
 }
